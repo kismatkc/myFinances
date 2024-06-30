bc9682db4712a5b56cccb7f5a9357ed394c6a3f1
kismatkc <kismatkc28@gmail.com>
2024-06-30 02:55:38 -0400

    set up mongodb

---
+++ backendmyfinances/server.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from "cors";
+import mongoose from "mongoose";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
+
+dotenv.config();
+const app = express();
+if (!process.env.DATABASE_URL) {
+  throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.DATABASE_URL as string)
+      .then((mongoose) => {
+        console.log('Connected to MongoDB');
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+        throw new Error('MongoDB connection error');
+      });
+    cached.conn = cached.promise;
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+const port = process.env.PORT || 4200;
+
+app.use(cors());
+
+app.use(ClerkExpressRequireAuth());
+
+
+
+
+
+app.get("/api", (req, res) => {
+  res.send("Hello from /api rout");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
