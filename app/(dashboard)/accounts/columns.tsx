a4c4636359260ba9bcf0899a39abfc1d6619125e
kismatkc <52825603+kismatkc@users.noreply.github.com>
2024-07-15 11:35:37 +0000

    auto

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -6,12 +6,7 @@ import { Button } from "@/components/ui/button";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
+
 import { Checkbox } from "@/components/ui/checkbox";
 export const columns: ColumnDef<Payment>[] = [
   {
@@ -36,26 +31,20 @@ export const columns: ColumnDef<Payment>[] = [
     enableSorting: false,
     enableHiding: false,
   },
+
   {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
+    accessorKey:"Name",
+    header: ({ name }) => {
       return (
         <Button
           variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          onClick={() => column.toggleSorting(name.getIsSorted() === "asc")}
         >
-          Email
+        Name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
     },
   },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
+
 ];
--- backendmyfinances/controllers/account-controller.js
+++ backendmyfinances/controllers/account-controller.js
@@ -1,16 +1,21 @@
 import Account from "../models/account-model.js";
-let newAccount= {};
-let createdUser = {};
+import accountData from '../data/account.js';
+import fs from "fs"
+import path from "path"
+
+const fullPathToData = path.join(__dirname,"../data/account.js");
+
+const writeToFile = (data)=>{
+    fs.writeFileSync(fullPathToData,`export default ${JSON.stringify(data,null,2)};`)
+}
+
 export const getUser = async(req,res)=>{
 
 
     try {
    
-         newAccount = {
-            a: "5",
-            b: "6"
-        }
-        res.status(200).json(newAccount);
+      
+        res.status(200).json(accountData);
 
     } catch (error) {
         res.status(500).json({error: error})
@@ -22,7 +27,7 @@ export const createUser = async(req,res)=>{
 try {
 
 createdUser = req.body
-    console.log(createdUser)
+    writeToFile(createUser)
 res.status(200).json(createdUser);
 } catch (error) {
     console.log("data not recievd")
