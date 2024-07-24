748834a68f26b4440ca3de276480befb88c34e83
kismatkc <52825603+kismatkc@users.noreply.github.com>
2024-07-24 12:36:02 +0000

    auto

--- backendmyfinances/data/account.js
+++ backendmyfinances/data/account.js
@@ -1,18 +1,1 @@
-export default [
-   {
-      "name": "kismat",
-      "id": "1"
-   },
-   {
-      "name": "anish",
-      "id": "2"
-   },
-   {
-      "name": "jdjsjs",
-      "id": "12"
-   },
-   {
-      "name": "hsjjswkkw",
-      "id": "11"
-   }
-];
+export default [];
